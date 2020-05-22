cc338d65821f28f25b46e994721f1fd94a1f4e23
OOXXXX <zhupx2003@126.com>
2020-05-22 08:25:57 +0800

    Well done! Make UI ProgressBar available

--- PastPaperO-Test/WebView.swift
+++ PastPaperO-Test/WebView.swift
@@ -12,34 +12,64 @@ import WebKit
 
 var activityIndicator: UIActivityIndicatorView!
 
-struct Webview:  UIViewRepresentable {
-    
-    var url: String
-    
-    func makeUIView(context: Context) -> WKWebView {
-        
-        guard let url = URL(string: self.url) else {
-            return WKWebView()
-            
+struct Webview: UIViewControllerRepresentable {
+    let url: String
+
+    func makeUIViewController(context: Context) -> WebviewController {
+        let webviewController = WebviewController()
+
+        let request = URLRequest(url: URL(string: self.url)!, cachePolicy: .returnCacheDataElseLoad)
+        webviewController.webview.load(request)
+
+        return webviewController
+    }
+
+    func updateUIViewController(_ webviewController: WebviewController, context: Context) {
+        let request = URLRequest(url: URL(string: self.url)!, cachePolicy: .returnCacheDataElseLoad)
+        webviewController.webview.load(request)
+    }
+}
+class WebviewController: UIViewController {
+    lazy var webview: WKWebView = WKWebView()
+    lazy var progressbar: UIProgressView = UIProgressView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.webview.frame = self.view.frame
+        self.view.addSubview(self.webview)
+
+        self.view.addSubview(self.progressbar)
+        self.progressbar.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addConstraints([
+            self.progressbar.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.progressbar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.progressbar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+        ])
+
+        self.progressbar.progress = 0.1
+        webview.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+    }
+
+    // MARK: - Web view progress
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        switch keyPath {
+        case "estimatedProgress":
+            if self.webview.estimatedProgress >= 1.0 {
+                UIView.animate(withDuration: 0.3, animations: { () in
+                    self.progressbar.alpha = 0.0
+                }, completion: { finished in
+                    self.progressbar.setProgress(0.0, animated: false)
+                })
+            } else {
+                self.progressbar.isHidden = false
+                self.progressbar.alpha = 1.0
+                progressbar.setProgress(Float(self.webview.estimatedProgress), animated: true)
+            }
+        default:
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
         }
-        
-        
-        
-        let request = URLRequest(url: url)
-        let wkWebview = WKWebView()
-        wkWebview.load(request)
-        return wkWebview
-        
-        
     }
-    
-    
-    func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<Webview>) {
-        
-        
-        
-    }
-    
 }
 
-// You can change the SafariServices to WebView in "OLEngListview" and I've already wrote the code.
+
